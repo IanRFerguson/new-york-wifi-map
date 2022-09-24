2a7e65f962b114510d394ae42e9265fd4ff0a913
Ian <47256454+IanRFerguson@users.noreply.github.com>
2022-09-23 22:24:30 -0400

    push to csv

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,16 @@
 #!/bin/python3
-from re import sub
-from flask import render_template, request, Flask
-from .helper.db import (init_db, read_db, 
-                        validate_input, push_to_db)
-from .helper.map import MapHandler
 import os, pathlib
+from time import sleep
+from flask import (render_template, request, 
+                   Flask, redirect, send_file,
+                   url_for, flash, after_this_request)
+
+from .helper.map import MapHandler
+from .helper.db import (handle_request, init_db, read_db)
+
 
 ##########
+
 
 app = Flask(__name__)
 app_path = pathlib.Path(__file__).parent
@@ -16,13 +20,16 @@ if not os.path.exists(os.path.join(app_path, "nyc.db")):
 
 print("\n** App Running **\n")
 
-handler = MapHandler()
-handler.build_map()
 
 ##########
 
+
 @app.route("/", methods=["GET", "POST"])
 def index():
+
+    if not os.path.exists("./app/templates/hotspots.html"):
+        dummy = open("./app/templates/hotspots.html", "w")
+        dummy.close()
 
     return render_template("index.html")
 
@@ -42,26 +49,33 @@ def add():
 
         address_string = f"{address_}, {city_}, NY"
 
-        validate_input(
-            address=address_string,
-            label_=venue_,
-            submitted_by=submitted_by,
-            comments=comments,
-            push_directly=True
+        try:
+            handle_request(
+                address=address_string,
+                label_=venue_,
+                submitted_by=submitted_by,
+                comments=comments,
+                venue_type=venue_type_
+            )
+
+        except Exception as e:
+            flash(e)
+            return redirect(url_for('index'))
+
+        return render_template(
+            "success.html",
+            address=address_string.upper()
         )
 
     return render_template("add.html")
 
 
+
 @app.route("/view_hotpots", methods=["GET", "POST"])
 def view():
-
     data = read_db()
 
-    if len(data) == 0:
-        data = None
-
-    return render_template("view.html", data=data)
+    return render_template("view.html", data=data.values.tolist())
 
 
 
@@ -71,6 +85,34 @@ def hotspots():
 
 
 
-# --- Run
+@app.route("/download_csv", methods=["GET", "POST"])
+def download_csv():
+    data = read_db()
+
+    output_path = os.path.join("./.temp_cache")
+    pathlib.Path(output_path).mkdir(parents=True, exist_ok=True)
+
+    data.to_csv(
+        os.path.join(output_path, "NYC_WIFI_HOTSPOTS.csv"),
+        index=False
+    )
+
+
+    @after_this_request
+    def remove_output(response_):
+        os.remove(
+            os.path.join(output_path, "NYC_WIFI_HOTSPOTS.csv")
+        )
+
+
+    return send_file(
+        os.path.join(output_path, "NYC_WIFI_HOTSPOTS.csv"),
+        as_attachment=True
+    )
+
+
+##########
+
+
 if __name__ == "__main__":
     app.run(debug=True)
